49cb82f477300481263616e030c198069b116ec4
Theezr <nickvanrossenberg@hotmail.com>
2024-04-25 15:29:22 +0200

    Dockerfile

--- guides/token-factory/backend/src/app/plugins.ts
+++ guides/token-factory/backend/src/app/plugins.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
+import { DashboardPlugin } from '@klayr/dashboard-plugin';
 import { Application } from 'klayr-sdk';
 
-export const registerPlugins = (_app: Application): void => {};
+export const registerPlugins = (app: Application): void => {
+	app.registerPlugin(new DashboardPlugin());
+};
