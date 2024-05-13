9bb4db9a1814d1fbf4906b1c00a9749857efdff4
franka <f.beekman.mmh@gmail.com>
2024-05-13 11:00:16 +0200

    env fix

--- guides/token-factory/frontend/utils/constants.ts
+++ guides/token-factory/frontend/utils/constants.ts
@@ -6,6 +6,6 @@ export const LISK32_CHARSET = 'zxvcpmbn3465o978uyrtkqew2adsjhfg'
 export const GENERATOR = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
 
 //WalletConnect
-export const projectID = process.env.REACT_APP_WC_PROJECT_ID as string;
+export const projectID = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string;
 export const currentChain = "lisk:01000000";
 export const chains = ["lisk:01000000"];
