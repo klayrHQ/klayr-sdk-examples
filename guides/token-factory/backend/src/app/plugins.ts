c734c23f9bbd4558fed333f778426f3f098bdde3
Theezr <nickvanrossenberg@hotmail.com>
2024-05-15 23:30:07 +0200

    dashboard

--- guides/token-factory/backend/src/app/plugins.ts
+++ guides/token-factory/backend/src/app/plugins.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'klayr-sdk';
 import { TokenFactoryInfoPlugin } from './plugins/token_factory_info/token_factory_info_plugin';
+import { DashboardPlugin } from '@klayr/dashboard-plugin';
 
 export const registerPlugins = (app: Application): void => {
 	app.registerPlugin(new TokenFactoryInfoPlugin());
+	app.registerPlugin(new DashboardPlugin());
 };
