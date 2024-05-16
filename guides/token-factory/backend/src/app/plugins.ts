8f8a89547c7de426b5dbb1bf015a6a5478ea98b3
Theezr <nickvanrossenberg@hotmail.com>
2024-05-16 09:28:50 +0200

    token-factory faucet

--- guides/token-factory/backend/src/app/plugins.ts
+++ guides/token-factory/backend/src/app/plugins.ts
@@ -2,8 +2,10 @@
 import { Application } from 'klayr-sdk';
 import { TokenFactoryInfoPlugin } from './plugins/token_factory_info/token_factory_info_plugin';
 import { DashboardPlugin } from '@klayr/dashboard-plugin';
+import { FaucetPlugin } from '@klayr/faucet-plugin';
 
 export const registerPlugins = (app: Application): void => {
 	app.registerPlugin(new TokenFactoryInfoPlugin());
 	app.registerPlugin(new DashboardPlugin());
+	app.registerPlugin(new FaucetPlugin());
 };
