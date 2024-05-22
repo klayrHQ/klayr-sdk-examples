7388ba0f20474a2561ded81129f6b208a761882b
Franka <f.beekman.mmh@gmail.com>
2024-05-22 17:58:06 +0200

    Finish mint tokens transaction

--- guides/token-factory/frontend/components/tokens/actions/mint.tsx
+++ guides/token-factory/frontend/components/tokens/actions/mint.tsx
@@ -1,12 +1,85 @@
 import { Button, Grid, Input, InputLabel, Stack, Typography } from '@mui/material';
 import { tokenActionsProps, TxsProps } from '@/components/tokens/tokenActionsModal';
 import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import { TransactionModal } from '@/components/walletConnect/transactionModal';
+import { useWalletConnect } from '@/providers/walletConnectProvider';
+import { useSchemas } from '@/providers/schemaProvider';
+import { useEffect, useState } from 'react';
+import { TransactionStatus } from '@/types/transactions';
+import { createTransactionObject, returnIfString } from '@/utils/functions';
+import { api } from '@/utils/api';
 
 export const Mint = ({ tokenID, tokenName }: tokenActionsProps) => {
 	const { register, handleSubmit, formState: { errors } } = useForm({defaultValues: {tokenID}});
+	const { account, signTransaction, rpcResult } = useWalletConnect();
+	const { getSchema } = useSchemas();
+	const [openTransactionModal, setOpenTransactionModal] = useState<boolean>(false);
+	const [transactionModalType, setTransactionModalType] = useState<"approve" | "status">("status");
+	const [transactionStatus, setTransactionStatus] = useState<TransactionStatus>(TransactionStatus.PENDING);
 
-	const onSubmit: SubmitHandler<TxsProps> = (data) => console.log(data);
+	const onSubmit: SubmitHandler<TxsProps> = (data) => {
+		if(account) {
+			const { chainID, publicKey } = account;
+			const schema = getSchema(false, "tokenFactory:mint")
+
+			createTransactionObject("mint", account, data)
+				.then(({ transactionObject: rawTx, }) => {
+					//console.log('transaction: ', rawTx);
+					signTransaction(chainID, publicKey, schema, rawTx);
+					setTransactionModalType("status")
+					setOpenTransactionModal(true);
+				})
+				.catch(error => {
+					console.log(error)
+				});
+		}
+	}
+
+	useEffect(() => {
+		if (rpcResult && rpcResult.valid) {
+			setTransactionModalType("approve");
+			if (!openTransactionModal) setOpenTransactionModal(true);
+		}
+	}, [rpcResult]);
+
+	//submit signed transaction
+	const onConfirmApproval = () => {
+		if (rpcResult?.result) {
+			console.log(rpcResult)
+			api.post("transactions", { transaction: rpcResult.result })
+				.then(r => {
+					if(r.error) {
+						setTransactionStatus(TransactionStatus.FAILURE);
+						setTransactionModalType("status");
+					}
+					else {
+						setTransactionStatus(TransactionStatus.SUCCESS);
+						setTransactionModalType('status');
+					}
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		}
+	};
+
 	const onError: SubmitErrorHandler<TxsProps> = (errors) => console.log(errors);
+
+	const getErrorText = (errorType: string | undefined, fieldType?: string | undefined) => {
+		let errorText = "Unknown input error";
+
+		if (errorType === "required") errorText = "This field is required";
+
+		if (errorType === "pattern") {
+			if(fieldType === "number") {
+				errorText = "This field only accepts numbers";
+			} else {
+				errorText = "Invalid input";
+			}
+		}
+
+		return <Typography className={"text-xs text-[#FF422D] mt-1"}>{errorText}</Typography>;
+	}
 
 	return (
 		<Stack className={"gap-8 min-h-full"}>
@@ -20,8 +93,9 @@ export const Mint = ({ tokenID, tokenName }: tokenActionsProps) => {
 								className={'w-full'}
 								type={'text'}
 								placeholder={'Amount to mint'}
-								{...register("amount", {required: true})}
+								{...register("amount", {required: true, pattern: /^[0-9]+$/i})}
 							/>
+							{errors.amount && getErrorText(returnIfString(errors.amount.type), "number")}
 						</InputLabel>
 					</Grid>
 					<Grid item className={'flex items-end'}>
@@ -31,6 +105,14 @@ export const Mint = ({ tokenID, tokenName }: tokenActionsProps) => {
 					</Grid>
 				</Grid>
 			</form>
+			<TransactionModal
+				modalType={transactionModalType}
+				type={"batchTransfer"}
+				open={openTransactionModal}
+				onClose={() => setOpenTransactionModal(false)}
+				onApprove={onConfirmApproval}
+				status={transactionStatus}
+			/>
 		</Stack>
 	)
 }
